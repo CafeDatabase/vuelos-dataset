faa21584b6038bee991ba607c6e2016b8fe4fd6c
Café Database <javier@cafedatabase.com>
2026-02-04 16:23:35 +0100

    Eliminar rutas de mi PC

--- sql/objects/vuelos.sql
+++ sql/objects/vuelos.sql
@@ -1,5 +1,3 @@
--- C:\TechLevel\formacion\manuales TechLevel\análisis y diseño de bbdd\WorkShop\vuelos.sql
---
 -- Generated for Oracle 9i on Mon May 17  01:47:37 2004 by Server Generator 9.0.2.80.4
 
 
